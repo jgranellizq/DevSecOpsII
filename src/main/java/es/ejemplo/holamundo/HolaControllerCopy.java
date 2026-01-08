d4e2b346468ed54af371ee4978f7d0d52695a595
jgranellizq <94468063+jgranellizq@users.noreply.github.com>
2026-01-08 18:48:35 +0100

    Create HolaControllerCopy.java

---
+++ src/main/java/es/ejemplo/holamundo/HolaControllerCopy.java
@@ -0,0 +1,28 @@
+
+package es.ejemplo.holamundo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HolaController {
+
+    @GetMapping("/hola")
+    public Map<String, String> hola() {
+        return Map.of("mensaje", "Hola Mundo desde Spring Boot en Docker");
+    }
+
+    @GetMapping("/health")
+    public Map<String, String> health() {
+        return Map.of("status", "UP");
+    }
+
+    @GetMapping("/holaplus")
+    public Map<String, String> holaplus() {
+        return Map.of("mensaje", "Hola de nuevo Mundo desde Spring Boot en Docker");
+    }
+}
+
+
